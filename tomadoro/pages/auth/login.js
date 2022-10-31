babea80d7c54bf12fb6e943b4df86c34cb90262e
Filip Swierczynski <swirek.gw@hotmail.com>
2022-10-31 17:30:34 -0400

    added use Effect

--- tomadoro/pages/auth/login.js
+++ tomadoro/pages/auth/login.js
@@ -2,9 +2,12 @@ import {FcGoogle} from 'react-icons/fc';
 import {signInWithPopup,GoogleAuthProvider} from 'firebase/auth';
 import {auth} from "../../utils/firebase";
 import {useRouter} from 'next/router';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import { useEffect } from 'react';
 
 export default function Login (){
 const route = useRouter();
+const [user,loading] = useAuthState(auth);
 // sign in with google
 const googleProvider = new GoogleAuthProvider();
 const GoogleLogin = async() => {
@@ -15,6 +18,14 @@ const GoogleLogin = async() => {
         console.log(error);
     }
 }
+
+useEffect(()=> {
+    if(user){
+        route.push('/');
+    } else{
+        console.log('login');
+    }
+},[user])
     return (
         <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
             <h2 className="text-2xl font-medium">Join Today</h2>
